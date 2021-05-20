5cff96bf20847f4589f3bcafae58793351704a31
Daniel" "Cook <dcoo8782@MXLAB.ACFR.USYD.EDU.AU>
2021-05-20 10:50:30 +1000

    Updated to make interups work apparently

---
+++ Timer_Interrupts/Sources/Functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_HEADER
+#define FUNCTIONS_HEADER
+
+void enableTimer(int cr1, int cr2);
+//int convertTimerToTime(int prescalar, unsigned timer, float* output);
+void init_TC1(void);
+void TCNTOF(unsigned enable);
+__interrupt void TOV_ISR(void);
+__interrupt void TC1_ISR(void);
+
+#endif

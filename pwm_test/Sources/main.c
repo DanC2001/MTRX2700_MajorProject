c1903eb21e5d5c2d12ad1c7c0df23183eed4c4f2
Andy Scott <andy.scott217@gmail.com>
2021-05-26 22:43:14 +1000

    Got servos working and scanning area

--- pwm_test/Sources/main.c
+++ pwm_test/Sources/main.c
@@ -1,19 +1,142 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 
+/** The minimuim duty cycle allowed by the servos */
+#define MIN_DUTY_CYCLE 2700
+/** The maximium duty cycle allowed by the servos */
+#define MAX_DUTY_CYCLE 6300
+/** The period of the PWM signal */
+#define PERIOD 60000
 
+// Function to move servos
+unsigned char move_servos(float tilt, float pan){
+	
+	unsigned int tilt_duty_cycle , pan_duty_cycle;
+	
+	if( tilt>180.0 || tilt<0.0 || pan>180.0 || pan<0.0 ) return 0;
+	tilt_duty_cycle = MIN_DUTY_CYCLE + (20 * tilt);
+	pan_duty_cycle = MIN_DUTY_CYCLE + (20 * pan);
+		
+//	assert(elevationDutyCycle <= MAX_DUTY_CYCLE && elevationDutyCycle >= MIN_DUTY_CYCLE && azimuthDutyCycle <= MAX_DUTY_CYCLE && azimuthDutyCycle >= MIN_DUTY_CYCLE);
+		
+	PWMDTY45 = tilt_duty_cycle;
+	PWMDTY67 = pan_duty_cycle;
+	return 1;
+}
 
-void main(void) 
-{
+// Function to initialise servos
+void initialise_servos(void){
 
+  // Configure Port P data direction register for output
+	DDRP = 0xFF;
+	
+	// Set each concatenated period channel to the calculated period value
+	PWMPER45 = PERIOD;
+	PWMPER67 = PERIOD;
+
+  // Set each duty cycle to be minimum plus 90 degrees so that servos initially start from central location
+	PWMDTY45 = MIN_DUTY_CYCLE + (20 * 90);
+	PWMDTY67 = MIN_DUTY_CYCLE + (20 * 90);
+}
+
+// Function to initialise PWM settings
+void initialise_PWM(int clock_A , int clock_B){
+  
+  // Set the prescalers of clock A and B
+	PWMPRCLK = clock_A + (clock_B << 4);
+  
+  // Concatenate channels 4 and 5 to create a 16 bit channel
+  PWMCTL_CON45 = 0x01;
+  
+  // Concatenate channels 6 and 7 to create a 16 bit channel
+  PWMCTL_CON67 = 0x01;
+  
+  // Configure pulse to be left aligned
+	PWMCAE = 0x00;
+	
+	// Set polarity to 1
+	PWMPOL = 0xFF;
+	
+	// Enable PWM channels
+	PWME = 0xF0;
+}
+
+// Function causes time delay of 1 millisecond * input parameter
+void ms_delay(unsigned int time_ms){
+
+    // Define counter variables to count back from
+    // Volatile helps ensure this is not optimised out by compiler
+    volatile int i; 
+    volatile int j;
+    
+    // Define length of delay required for 1 ms
+    volatile int ms_delay = 4000;
+    
+    // Waste time for number of inputted milliseconds
+    for(i = 0; i < time_ms ; i++) {
+      
+      for(j = 0 ; j <ms_delay ; j++){
+      }
+    }
+}
+
+void main(void) {
+    
+	
+  	// Define tilt and pan scan bounds passed of design requirements, includes calibration adjustments
+  	unsigned char min_tilt = 60;
+  	unsigned char max_tilt = 100;
+  	unsigned char min_pan = 90;
+  	unsigned char max_pan = 130;
+  	
+  	// Define increment to increase pan and tilt by
+  	unsigned char increment = 2;
+  	unsigned char i,j;
+  	
+  	
+  	// Initialise PWM module with a prescaler of 2^3 for both clock A and clock B
+  	initialise_PWM(3,3);
+  	
+  	// Initialise servos
+  	initialise_servos();
+  		
+  	// Enable interrupts for lidar
+  	EnableInterrupts;
+	
+  	// Scan indefinitely
+  	while(1){ 	  
+    
+      // Loop through tilt angles
+      for(i = 0; i < 5; i++){
+      
+        // Loop through panning angles
+        for(j = 0; j < 5; j++){
+          
+          // Move servo to calculated position
+          move_servos(min_tilt +  i*increment, max_tilt + j*increment);
+          
+          // Pause for half a second
+          ms_delay(100);
+          
+        }
+        
+      }
+      
+      // Feed the dog after each scan to avoid timeout reset
+      _FEED_COP();
+      
+  	}
+    
+ 
   /*int max_period = 2000;
   int min_period = 1000;
   int right_moving_flag = 1;
   int increment = 100;
-  float current_period;  */
+  float current_period;
+  
+      
   
   
-  /* put your own code here */
   int duty_cycle = 50;
   int target_freq = 800; //Hz //15000 should give resulting freq of 800hz = 1.3ms, 20000 should give 600hz = 1.7ms
   int clock_freq = 12*1000*1000; // Clock is 24Mhz/prescaler = 24e6/2
@@ -86,8 +209,9 @@ void main(void)
       }     
       
     }*/
+    
+    
 
-	EnableInterrupts;
 
 
   for(;;) {
